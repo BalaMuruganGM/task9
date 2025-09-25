c32a16a083d8123a1f1c86aebb4dd4809a70c81e
BalaMuruganGM <balagm56@gmail.com>
2025-09-25 07:26:43 -1200

    new2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Form from './Component/Form';
 
-const App = ()=>{
-    return(
-        <Form/>
-    )
-    
-}
+const App = () => {
+  return (
+    <BrowserRouter basename="/BalaMuruganGM/task9">
+      <Routes>
+        <Route path="/" element={<Form />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import ReactDom from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
-const root = ReactDom.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<App/>} />
-                </Routes>
-        </BrowserRouter>
-    </React.StrictMode>
-)
+<React.StrictMode>
+      <App />
+  </React.StrictMode>
+  
+);
